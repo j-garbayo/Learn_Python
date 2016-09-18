ddca91971ed4ab76c0a530914a51d27960d0a835
Juan Garbayo <j.garbayo.pablo@gmail.com>
2016-09-18 15:34:37 +0200

    Upddate

---
+++ Learn_Python/040_Tuples.py
@@ -0,0 +1,44 @@
+'''
+In Python tuples are very similar to lists, however, unlike lists they are *immutable* meaning they can not be changed. You would use tuples to present things that shouldn't be changed, such as days of the week, or dates on a calendar. 
+
+In this section, we will get a brief overview of the following:
+
+    1.) Constructing Tuples
+    2.) Basic Tuple Methods
+    3.) Immutability
+    4.) When to Use Tuples.
+
+You'll have an intuition of how to use tuples based on what you've learned about lists. We can treat them very similarly with the major distinction being that tuples are immutable.
+
+## Constructing Tuples
+
+The construction of a tuples use () with elements separated by commas. For example:
+'''
+
+# Can create a tuple with mixed types
+t = (1,2,3)
+
+# Check len just like a list
+print(len(t))
+
+# Can also mix object types
+t = ('one',2)
+
+# Show
+print(t)
+
+# Use indexing just like we did in lists
+print(t[0])
+
+# Slicing just like a list
+print "t[-1]"
+print(t[-1])
+
+print("")
+print("BASIC TUPLE METHODS")
+
+# Use .index to enter a value and return the index
+print("t.index('one') = " + str(t.index('one'))
+
+# Use .count to count the number of times a value appears
+print("t.count('one') = " + str(t.count('one'))
